88ea211f08497f26c177d24c0989586787f4e98f
Gaetan de Villele <gdevillele@gmail.com>
2026-06-19 19:47:41 -0700

    add Xcode project

---
+++ app/apple/herm/hermApp.swift
@@ -0,0 +1,17 @@
+//
+//  hermApp.swift
+//  herm
+//
+//  Created by Gaetan de Villele on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct hermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
